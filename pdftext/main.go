2bb65eb840218bc44541d169417f12fc31280756
Jim Grier <jimgrier1@gmail.com>
2019-03-26 20:33:59 -0400

    Add test pdf file

--- pdftext/main.go
+++ pdftext/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "grier/pdftext"
+import (
+	"grier/pdftext"
+
+	"github.com/pkg/profile"
+)
 
 func main() {
-	//	defer profile.Start(profile.MemProfile, profile.CPUProfile, profile.ProfilePath(".")).Stop()
+	defer profile.Start(profile.MemProfile, profile.CPUProfile, profile.ProfilePath(".")).Stop()
 
 	pdftext.Run()
 }
